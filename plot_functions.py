127590fd0bad11adb19ba69e7ae5120e13af6d6d
Maria Carolina Gonzalez Galtier <53412617+carogaltier@users.noreply.github.com>
2025-08-04 13:39:24 -0300

    Update plot_functions.py

--- plot_functions.py
+++ plot_functions.py
@@ -409,7 +409,7 @@ def plot_scheduling_interval_distribution(df):
     fig.patches.extend([rect])
     
     scheduling_intervals = df['scheduling_interval']
-    bins = range(scheduling_intervals.min(), scheduling_intervals.max() +2 )
+    bins = range(scheduling_intervals.min(), scheduling_intervals.max() +20 )
     counts, edges = np.histogram(scheduling_intervals, bins=bins)
     percentages = (counts / scheduling_intervals.size) * 100
     
@@ -635,4 +635,5 @@ def plot_arrival_time_distribution(df):
             color='#222', ha='center'
         )
     plt.show()
+
 

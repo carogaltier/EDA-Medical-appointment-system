4b77781eed9e4e1f6d6ba7ba1a5fa0e0f4be81d7
Maria Carolina Gonzalez Galtier <53412617+carogaltier@users.noreply.github.com>
2025-08-07 15:53:07 -0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,31 @@
+DEFAULT_AGE_GENDER_PROBS = [
+    {"age_yrs": "0-4", "total_female": 0.01413, "total_male": 0.01794},
+    {"age_yrs": "5-9", "total_female": 0.01140, "total_male": 0.01409},
+    {"age_yrs": "10-14", "total_female": 0.01318, "total_male": 0.01459},
+    {"age_yrs": "15-19", "total_female": 0.01738, "total_male": 0.01348},
+    {"age_yrs": "20-24", "total_female": 0.02326, "total_male": 0.01010},
+    {"age_yrs": "25-29", "total_female": 0.03988, "total_male": 0.01208},
+    {"age_yrs": "30-34", "total_female": 0.05164, "total_male": 0.01449},
+    {"age_yrs": "35-39", "total_female": 0.04369, "total_male": 0.01591},
+    {"age_yrs": "40-44", "total_female": 0.03240, "total_male": 0.01754},
+    {"age_yrs": "45-49", "total_female": 0.02902, "total_male": 0.01861},
+    {"age_yrs": "50-54", "total_female": 0.03684, "total_male": 0.02513},
+    {"age_yrs": "55-59", "total_female": 0.04172, "total_male": 0.03249},
+    {"age_yrs": "60-64", "total_female": 0.04188, "total_male": 0.03723},
+    {"age_yrs": "65-69", "total_female": 0.03939, "total_male": 0.03822},
+    {"age_yrs": "70-74", "total_female": 0.04026, "total_male": 0.03995},
+    {"age_yrs": "75-79", "total_female": 0.04395, "total_male": 0.04334},
+    {"age_yrs": "80-84", "total_female": 0.03090, "total_male": 0.02876},
+    {"age_yrs": "85-89", "total_female": 0.02015, "total_male": 0.01745},
+    {"age_yrs": "90+", "total_female": 0.01040, "total_male": 0.00716},
+]
+
+
+DEFAULT_STATUS_RATES = {
+    "attended": 0.773,
+    "cancelled": 0.164,
+    "did not attend": 0.059,
+    "unknown": 0.004
+}
+
+DEFAULT_FIRST_ATTENDANCE_RATIO = 0.325
